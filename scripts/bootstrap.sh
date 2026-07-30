7887e8a7be512fc0da839b40380f83c2a0571c38
Craig Frayne <craigfrayne@gmail.com>
2026-07-30 22:42:46 +0200

    adding bootstrap starter

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -e ".[dev]"
+
+echo
+echo "Environment ready."
